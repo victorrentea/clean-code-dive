3546634f5cc658cbd8be1a22b1bbdc5748595dc4
Victor Rentea <victorrentea@gmail.com>
2019-11-05 11:22:19 +0200

    initial commit before Devoxx BE 2019

---
+++ src/main/java/cleancode/BouleanParameters.java
@@ -0,0 +1,52 @@
+package cleancode;
+
+import java.util.List;
+
+@SuppressWarnings("all")
+public class BouleanParameters {
+
+	public static void main(String[] args) {
+		// The method is called from various foreign places in the codebase
+		bigUglyMethod(1, 2);
+		bigUglyMethod(1, 2);
+		bigUglyMethod(1, 2);
+		bigUglyMethod(1, 2);
+		bigUglyMethod(1, 2);
+
+		// TODO From my use-case, I call it too, to do more within:
+		bigUglyMethod(1, 2);
+
+	}
+
+	static void bigUglyMethod(int a, int b) {
+		System.out.println("Complex Logic");
+		System.out.println("Complex Logic");
+		System.out.println("Complex Logic");
+
+		System.out.println("More Complex Logic");
+		System.out.println("More Complex Logic");
+		System.out.println("More Complex Logic");
+	}
+
+	// ============== "BOSS" LEVEL: A lot harder to break down =================
+
+	static void bossLevel(boolean stuff, boolean fluff, List<Integer> tasks) {
+		int i = 0;
+		int j = 1;
+		System.out.println("Logic1");
+		if (stuff) {
+			System.out.println("Logic2");
+			if (fluff) {
+				System.out.println("Logic3");
+				for (int task : tasks) {
+					i++;
+					System.out.println("Logic4 " + task);
+					// TODO HERE, when call this method, I want MY own custom code to run here
+					System.out.println("Logic5 " + i);
+				}
+				System.out.println("Logic6 " + j++);
+			}
+		}
+		System.out.println("Logic7");
+	}
+}
---
+++ src/main/java/cleancode/ManyParamsVO.java
@@ -0,0 +1,52 @@
+package cleancode;
+
+public class ManyParamsVO {
+    public static void main(String[] args) {
+        new ManyParamsVO().placeOrder("John", "Doe", "St. Albergue", "Paris", 99);
+    }
+    public void placeOrder(String fName, String lName, String city, String streetName, Integer streetNumber) {
+    	if (fName == null || lName == null) throw new IllegalArgumentException();
+    	
+    	System.out.println("Some Logic");
+    }
+}
+
+class AnotherClass {
+    public void otherMethod(String firstName, String lastName, int x) {
+    	if (firstName == null || lastName == null) throw new IllegalArgumentException();
+    	
+    	System.out.println("Another distant Logic");
+    }
+}
+
+class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if (firstName == null || lastName == null) throw new IllegalArgumentException();
+        // TODO think: is this sufficient enforcing ?
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+}
+
+class PersonService {
+    public void f(Person person) {
+        String fullName = person.getFirstName() + " " + person.getLastName().toUpperCase();
+        System.out.println(fullName);
+    }
+}
